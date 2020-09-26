da5cac4e45dec76c06320060363691cb7bf48078
Elizaveta Dolgova <dolgovadesign@gmail.com>
2020-09-25 22:16:42 -0400

    Adds initial source code for Exercise 1

---
+++ App.js
@@ -0,0 +1,30 @@
+
+import React from "react";
+import { NavigationContainer } from '@react-navigation/native';
+import { createStackNavigator } from '@react-navigation/stack';
+
+import { StyleSheet,
+         Text, 
+         View, 
+         Button, 
+         Alert, 
+         Image, 
+         Dimensions, 
+         AsyncStorage
+        } from "react-native";
+
+import Home from "./screens/Home";
+import Details from "./screens/Details";
+
+const Stack = createStackNavigator();
+
+export default function App() {
+  return (
+    <NavigationContainer>
+      <Stack.Navigator>
+        <Stack.Screen name="My Diary" component={Home} />
+        <Stack.Screen name="Photos" component={Details} />
+      </Stack.Navigator>
+    </NavigationContainer> 
+  );
+}
